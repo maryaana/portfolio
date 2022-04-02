ca7817be999540b38d818d37e2721f71f913f0c6
German Iskhakov <german@superdao.co>
2022-04-02 23:38:42 +0300

    feat: smooth inertial scrolling effect

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,7 @@
 import { MediaLinkSVG } from 'assets/svgs';
 import { Footer } from 'components/Footer/Footer';
 import { Subheading } from 'components/Subheading/Subheading';
-import { CSSProperties } from 'react';
+import { CSSProperties, useEffect } from 'react';
 import { EROUTES } from 'types/enums';
 import { Heading } from '../Heading/Heading';
 import styles from './About.module.scss';
@@ -12,112 +12,157 @@ type Props = {
 };
 
 export const About = ({ isActive, onNewRoute }: Props) => {
+  useEffect(() => {
+    const scroller = document.getElementById('aboutScroller')!;
+    const body = document.getElementById('aboutWrapper')!;
+    const main = document.getElementById('aboutInner')!;
+
+    let sx = 0, // For scroll positions
+      sy = 0;
+    let dx = sx, // For container positions
+      dy = sy;
+
+    body.style.height = main.clientHeight - window.innerHeight * 2 + 'px';
+
+    // Bind a scroll function
+    scroller.addEventListener('scroll', easeScroll);
+
+    function easeScroll() {
+      sx = scroller.scrollLeft;
+      sy = scroller.scrollTop;
+    }
+
+    window.requestAnimationFrame(render);
+
+    function render() {
+      //We calculate our container position by linear interpolation method
+      dx = li(dx, sx, 0.07);
+      dy = li(dy, sy, 0.07);
+
+      dx = Math.floor(dx * 100) / 100;
+      dy = Math.floor(dy * 100) / 100;
+
+      main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+
+      window.requestAnimationFrame(render);
+    }
+
+    function li(a: any, b: any, n: any) {
+      return (1 - n) * a + n * b;
+    }
+  }, []);
+
   return (
-    <section className={`${styles.wrapper} ${!isActive && styles.wrapper_hidden}`}>
-      <div className={styles.inner}>
-        <Heading content="Обо мне" className={styles.heading} />
-        <div className={styles.content}>
-          <div style={{ '--delay': 0 } as CSSProperties} className={styles.line}>
-            <div className={styles.sticky}>
-              <Subheading content="Интро" />
-            </div>
-            <div className={`${styles.text} ${styles.text_accent}`}>
-              Привет, я Марьяна Титова, web-дизайнер в Санкт-Петербурге. Я занимаюсь дизайном
-              интерфейсов и прототипированием. Постоянно ищу баланс между эстетикой, удобством для
-              пользователя и задачами бизнеса. Люблю продуктовый дизайн и минимализм (:
-            </div>
-          </div>
-          <div style={{ '--delay': 0.1 } as CSSProperties} className={styles.line}>
-            <div className={styles.sticky}>
-              <Subheading content="Опыт работы" />
-            </div>
-            <div className={styles.text}>
-              Три года на фрилансе: в основном делала дизайн и адаптивные версии корпоративных
-              сайтов, онлайн-магазинов, лендингов, мобильные приложения, редизайны продуктов. А ещё
-              немного логотипов, баннеров и листовок.
-            </div>
-          </div>
-          <div style={{ '--delay': 0.2 } as CSSProperties} className={styles.line}>
-            <div className={styles.sticky}>
-              <Subheading content="Образование" />
-            </div>
-            <div className={styles.text}>Университет ИТМО</div>
-          </div>
-          <div style={{ '--delay': 0.3 } as CSSProperties} className={styles.line}>
-            <div className={styles.sticky}>
-              <Subheading content="Что умею" />
-            </div>
-            <div className={styles.text}>
-              — Прототипирование и дизайн: Figma, Adobe XD
-              <br />
-              <br />
-              — Анимация: Motion и Figmotion (плагины Figma), Adobe After Effects, SVGator
-              (SVG-анимация)
-              <br />
-              <br />
-              — Иллюстрация: Adobe Illustrator, Adobe Photoshop
-              <br />
-              <br />
-              — Знаю фронтенд на базовом уровне (CSS, HTML, JS). Я программист по образованию, так
-              что могу общаться с командой разработчиков на одном языке :)
-              <br />
-              <br />
-              — Знакома с Human Interface Guidelines
-              <br />
-              <br />
-              — Немного рисую на графическом планшете (7 лет отучилась в художественной школе)
-              <br />
-              <br />— С удовольствием обучусь новым технологиям и работе с новыми инструментами
-            </div>
-          </div>
-          <div style={{ '--delay': 0.4 } as CSSProperties} className={styles.line}>
-            <div className={styles.sticky}>
-              <Subheading content="Опыт работы" />
-            </div>
-            <div className={styles.text}>
-              Три года на фрилансе: в основном делала дизайн и адаптивные версии корпоративных
-              сайтов, онлайн-магазинов, лендингов, мобильные приложения, редизайны продуктов. А ещё
-              немного логотипов, баннеров и листовок.
-            </div>
-          </div>
-          <div style={{ '--delay': 0.5 } as CSSProperties} className={styles.line}>
-            <div className={styles.sticky}>
-              <Subheading content="Медиа" />
-            </div>
-            <div className={styles.text}>
-              <div className={styles.mediaBox}>
-                <a target="_blank" href="https://www.behance.net/maryaana">
-                  <span>Behance</span> <MediaLinkSVG className={styles.mediaLink} />
-                </a>
+    <section
+      id="aboutScroller"
+      className={`${styles.wrapper} ${!isActive && styles.wrapper_hidden}`}
+    >
+      <div id="aboutWrapper">
+        <div id="aboutInner" className={styles.inner}>
+          <Heading content="Обо мне" className={styles.heading} />
+          <div className={styles.content}>
+            <div style={{ '--delay': 0 } as CSSProperties} className={styles.line}>
+              <div className={styles.sticky}>
+                <Subheading content="Интро" />
               </div>
-              <br />
-              <div className={styles.mediaBox}>
-                <a target="_blank" href="https://dribbble.com/maryaana">
-                  <span>Dribbble</span> <MediaLinkSVG className={styles.mediaLink} />
-                </a>
-              </div>
-              <br />
-              <div className={styles.mediaBox}>
-                <a target="_blank" href="https://www.linkedin.com/in/maryaana/">
-                  <span>Linkedin</span> <MediaLinkSVG className={styles.mediaLink} />
-                </a>
-              </div>
-              <br />
-              <div className={styles.mediaBox}>
-                <a target="_blank" href="https://t.me/maryaana">
-                  <span>Telegram</span> <MediaLinkSVG className={styles.mediaLink} />
-                </a>
+              <div className={`${styles.text} ${styles.text_accent}`}>
+                Привет, я Марьяна Титова, web-дизайнер в Санкт-Петербурге. Я занимаюсь дизайном
+                интерфейсов и прототипированием. Постоянно ищу баланс между эстетикой, удобством для
+                пользователя и задачами бизнеса. Люблю продуктовый дизайн и минимализм (:
               </div>
             </div>
+            <div style={{ '--delay': 0.1 } as CSSProperties} className={styles.line}>
+              <div className={styles.sticky}>
+                <Subheading content="Опыт работы" />
+              </div>
+              <div className={styles.text}>
+                Три года на фрилансе: в основном делала дизайн и адаптивные версии корпоративных
+                сайтов, онлайн-магазинов, лендингов, мобильные приложения, редизайны продуктов. А
+                ещё немного логотипов, баннеров и листовок.
+              </div>
+            </div>
+            <div style={{ '--delay': 0.2 } as CSSProperties} className={styles.line}>
+              <div className={styles.sticky}>
+                <Subheading content="Образование" />
+              </div>
+              <div className={styles.text}>Университет ИТМО</div>
+            </div>
+            <div style={{ '--delay': 0.3 } as CSSProperties} className={styles.line}>
+              <div className={styles.sticky}>
+                <Subheading content="Что умею" />
+              </div>
+              <div className={styles.text}>
+                — Прототипирование и дизайн: Figma, Adobe XD
+                <br />
+                <br />
+                — Анимация: Motion и Figmotion (плагины Figma), Adobe After Effects, SVGator
+                (SVG-анимация)
+                <br />
+                <br />
+                — Иллюстрация: Adobe Illustrator, Adobe Photoshop
+                <br />
+                <br />
+                — Знаю фронтенд на базовом уровне (CSS, HTML, JS). Я программист по образованию, так
+                что могу общаться с командой разработчиков на одном языке :)
+                <br />
+                <br />
+                — Знакома с Human Interface Guidelines
+                <br />
+                <br />
+                — Немного рисую на графическом планшете (7 лет отучилась в художественной школе)
+                <br />
+                <br />— С удовольствием обучусь новым технологиям и работе с новыми инструментами
+              </div>
+            </div>
+            <div style={{ '--delay': 0.4 } as CSSProperties} className={styles.line}>
+              <div className={styles.sticky}>
+                <Subheading content="Опыт работы" />
+              </div>
+              <div className={styles.text}>
+                Три года на фрилансе: в основном делала дизайн и адаптивные версии корпоративных
+                сайтов, онлайн-магазинов, лендингов, мобильные приложения, редизайны продуктов. А
+                ещё немного логотипов, баннеров и листовок.
+              </div>
+            </div>
+            <div style={{ '--delay': 0.5 } as CSSProperties} className={styles.line}>
+              <div className={styles.sticky}>
+                <Subheading content="Медиа" />
+              </div>
+              <div className={styles.text}>
+                <div className={styles.mediaBox}>
+                  <a target="_blank" href="https://www.behance.net/maryaana">
+                    <span>Behance</span> <MediaLinkSVG className={styles.mediaLink} />
+                  </a>
+                </div>
+                <br />
+                <div className={styles.mediaBox}>
+                  <a target="_blank" href="https://dribbble.com/maryaana">
+                    <span>Dribbble</span> <MediaLinkSVG className={styles.mediaLink} />
+                  </a>
+                </div>
+                <br />
+                <div className={styles.mediaBox}>
+                  <a target="_blank" href="https://www.linkedin.com/in/maryaana/">
+                    <span>Linkedin</span> <MediaLinkSVG className={styles.mediaLink} />
+                  </a>
+                </div>
+                <br />
+                <div className={styles.mediaBox}>
+                  <a target="_blank" href="https://t.me/maryaana">
+                    <span>Telegram</span> <MediaLinkSVG className={styles.mediaLink} />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
+          <Footer
+            actionPath={EROUTES.PORTFOLIO}
+            onNewRoute={onNewRoute}
+            parentIsActive={isActive}
+            className={styles.footer}
+          />
         </div>
       </div>
-      <Footer
-        actionPath={EROUTES.PORTFOLIO}
-        onNewRoute={onNewRoute}
-        parentIsActive={isActive}
-        className={styles.footer}
-      />
     </section>
   );
 };
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -15,83 +15,125 @@ type Props = {
 };
 
 export const Portfolio = ({ isActive, onNewRoute }: Props) => {
+  useEffect(() => {
+    const scroller = document.getElementById('scroller')!;
+    const body = document.getElementById('wrapper')!;
+    const main = document.getElementById('inner')!;
+
+    let sx = 0, // For scroll positions
+      sy = 0;
+    let dx = sx, // For container positions
+      dy = sy;
+
+    body.style.height = main.clientHeight - window.innerHeight * 2 + 'px';
+
+    // Bind a scroll function
+    scroller.addEventListener('scroll', easeScroll);
+
+    function easeScroll() {
+      sx = scroller.scrollLeft;
+      sy = scroller.scrollTop;
+    }
+
+    window.requestAnimationFrame(render);
+
+    function render() {
+      //We calculate our container position by linear interpolation method
+      dx = li(dx, sx, 0.07);
+      dy = li(dy, sy, 0.07);
+
+      dx = Math.floor(dx * 100) / 100;
+      dy = Math.floor(dy * 100) / 100;
+
+      main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+
+      window.requestAnimationFrame(render);
+    }
+
+    function li(a: any, b: any, n: any) {
+      return (1 - n) * a + n * b;
+    }
+  }, []);
+
   return (
-    <section id="luxy" className={`${styles.wrapper} ${!isActive && styles.wrapper_hidden}`}>
-      <div className={styles.inner}>
-        <Heading content="Портфолио" className={styles.heading} />
-        <div className={styles.projectsArea}>
-          <div className={styles.left}>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.BEHANCE}
-                backgroundColor="#0073DC"
-                link="https://www.behance.net/gallery/139814219/Cooking-school-for-kids-Website"
-                path={childrenOnKitchen}
-              />
+    <section id="scroller" className={`${styles.wrapper} ${!isActive && styles.wrapper_hidden}`}>
+      <div id="wrapper">
+        <div id="inner" className={styles.inner}>
+          <Heading content="Портфолио" className={styles.heading} />
+          <div className={styles.projectsArea}>
+            <div className={styles.left}>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.BEHANCE}
+                  backgroundColor="#0073DC"
+                  link="https://www.behance.net/gallery/139814219/Cooking-school-for-kids-Website"
+                  path={childrenOnKitchen}
+                />
+              </div>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.BEHANCE}
+                  backgroundColor="#101010"
+                  appearDelay={200}
+                  link="https://www.behance.net/gallery/138780827/Business-Area-Website-Concept"
+                  path={business}
+                />
+              </div>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.SITE}
+                  backgroundColor="#43B757"
+                  appearDelay={200}
+                  link="https://litemanager.org/"
+                  path={liteManager}
+                />
+              </div>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.FIGMA}
+                  backgroundColor="#2997EE"
+                  appearDelay={200}
+                  link="https://www.figma.com/file/F16vohrUm3Xl1tkLBQYzVV/%5BPortfolio%5D-Cars-rent?node-id=0%3A1"
+                  path={carsRent}
+                />
+              </div>
             </div>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.BEHANCE}
-                backgroundColor="#101010"
-                appearDelay={200}
-                link="https://www.behance.net/gallery/138780827/Business-Area-Website-Concept"
-                path={business}
-              />
-            </div>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.SITE}
-                backgroundColor="#43B757"
-                appearDelay={200}
-                link="https://litemanager.org/"
-                path={liteManager}
-              />
-            </div>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.FIGMA}
-                backgroundColor="#2997EE"
-                appearDelay={200}
-                link="https://www.figma.com/file/F16vohrUm3Xl1tkLBQYzVV/%5BPortfolio%5D-Cars-rent?node-id=0%3A1"
-                path={carsRent}
-              />
+            <div className={styles.right}>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.BEHANCE}
+                  backgroundColor="#121212"
+                  appearDelay={200}
+                  link="https://www.behance.net/gallery/138296035/Architecture-Studio-Website-Concept"
+                  path={architecture}
+                />
+              </div>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.FIGMA}
+                  backgroundColor="#F9FF7F"
+                  link="https://www.figma.com/proto/g7oGb7aatxmj1oVdMEes9w/%5BPortfolio%5D-Zenter-Family?page-id=0%3A1&node-id=2%3A1038&viewport=265%2C48%2C0.03&scaling=scale-down&starting-point-node-id=2%3A1038"
+                  path={flour}
+                />
+              </div>
+              <div className={styles.projectBounder}>
+                <Project
+                  projectCase={EPROJECTCASES.SITE}
+                  backgroundColor="#2A313C"
+                  appearDelay={200}
+                  link="https://pechenyice.github.io/quartetLanding/"
+                  path={clio}
+                />
+              </div>
             </div>
           </div>
-          <div className={styles.right}>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.BEHANCE}
-                backgroundColor="#121212"
-                appearDelay={200}
-                link="https://www.behance.net/gallery/138296035/Architecture-Studio-Website-Concept"
-                path={architecture}
-              />
-            </div>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.FIGMA}
-                backgroundColor="#F9FF7F"
-                link="https://www.figma.com/proto/g7oGb7aatxmj1oVdMEes9w/%5BPortfolio%5D-Zenter-Family?page-id=0%3A1&node-id=2%3A1038&viewport=265%2C48%2C0.03&scaling=scale-down&starting-point-node-id=2%3A1038"
-                path={flour}
-              />
-            </div>
-            <div className={styles.projectBounder}>
-              <Project
-                projectCase={EPROJECTCASES.SITE}
-                backgroundColor="#2A313C"
-                appearDelay={200}
-                link="https://pechenyice.github.io/quartetLanding/"
-                path={clio}
-              />
-            </div>
-          </div>
+          <Footer
+            actionPath={EROUTES.ABOUT}
+            onNewRoute={onNewRoute}
+            parentIsActive={isActive}
+            className={styles.footer}
+          />
         </div>
-        <Footer
-          actionPath={EROUTES.ABOUT}
-          onNewRoute={onNewRoute}
-          parentIsActive={isActive}
-          className={styles.footer}
-        />
       </div>
     </section>
   );
